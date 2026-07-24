7645087b0f6b93dc112fffa865669b106c4da70f
Greg Distiller <greg.distiller@uct.ac.za>
2026-07-24 15:56:11 +0200

    A whole bunch of files that were sitting on the pc

---
+++ 15FoldScen/Cluster/Sims/120Traps/OSGrids.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+#SBATCH --account=stats
+#SBATCH --partition=ada
+#SBATCH --nodes=1 --ntasks=20
+#SBATCH --time=400:00:00
+#SBATCH --job-name="Cluster OS 40"
+#SBATCH --mail-user=Greg.Distiller@uct.ac.za
+#SBATCH --mail-type=BEGIN,END,FAIL
+ 
+hostname
+module load R/R-4.5.2
+R --slave CMD BATCH OSGrids.R
