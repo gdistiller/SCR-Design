04843e188da3ef865b08432952d4c667c3629cfb
Greg Distiller <greg.distiller@uct.ac.za>
2026-07-24 12:22:56 +0200

    New files for extra LW 120 job on the cluster

---
+++ 15FoldScen/Cluster/Sims/120Traps/LWb.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+#SBATCH --account=stats
+#SBATCH --partition=ada
+#SBATCH --nodes=1 --ntasks=20
+#SBATCH --time=60:00:00
+#SBATCH --job-name="LWB Sims 120"
+#SBATCH --mail-user=Greg.Distiller@uct.ac.za
+#SBATCH --mail-type=BEGIN,END,FAIL
+ 
+hostname
+module load R/R-4.5.2
+R --slave CMD BATCH LWb.R
