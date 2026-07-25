213d55ee80de210c4684275677950a7cb0faa5b6
Greg Distiller <GregDistiller@uct.ac.za>
2026-07-25 13:32:11 +0200

    Files I can push

---
+++ ProcessSimsvK.R
@@ -0,0 +1,9 @@
+#July 26, processing results vs varying K factor
+
+load("Cluster/Sims/GridResults40Ka.RData")
+
+load("Cluster/Sims/GridResults40Kb.RData")
+
+load("Cluster/Sims/GridResults40Kc.RData")
+
+
